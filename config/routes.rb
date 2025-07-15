4040157c35c96782d4ef4787461de95968528099
Bruno Vetorazo <vetorazo@gmail.com>
2025-07-15 19:36:59 +0900

    Changed root to index

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create]
+  root to: 'lists#index'
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: [:destroy]
 end
